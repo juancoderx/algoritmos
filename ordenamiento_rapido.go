e83763206c536c85ecd173a3fa3c0ffd572fdcbb
Juan Alvarez <juancoderx@gmail.com>
2022-12-06 21:13:14 -0400

    correcion quicksort

--- ordenamiento_rapido.go
+++ ordenamiento_rapido.go
@@ -2,37 +2,35 @@ package main
 
 func quicksort(listadesordenada []int, izq int, der int) {
 	pivote := listadesordenada[izq]
-	// tomamos primer elemento como pivote
-	i := izq // i realiza la búsqueda de izquierda a derecha
+	i := izq
 	j := der
-	// j realiza la búsqueda de derecha a izquierda
+
 	var aux int
 
-	for i < j { // mientras no se crucen las búsquedas
+	for i < j {
 		for listadesordenada[i] <= pivote && i < j {
 			i++
 		}
-		// busca elemento mayor que pivote
+
 		for listadesordenada[j] > pivote {
 			j--
 		}
-		// busca elemento menor que pivote
-		if i < j { // si no se han cruzado
-			aux = listadesordenada[i] // los intercambia
+
+		if i < j {
+			aux = listadesordenada[i]
 			listadesordenada[i] = listadesordenada[j]
 			listadesordenada[j] = aux
 		}
 	}
 
-	listadesordenada[izq] = listadesordenada[j] // se coloca el pivote en su lugar de forma que tendremos
+	listadesordenada[izq] = listadesordenada[j]
 	listadesordenada[j] = pivote
 
-	// los menores a su izquierda y los mayores a su derecha
 	if izq < j-1 {
-		quicksort(listadesordenada, izq, j-1) // ordenamos subarray izquierdo
+		quicksort(listadesordenada, izq, j-1)
 	}
 
 	if j+1 < der {
-		quicksort(listadesordenada, j+1, der) // ordenamos subarray derecho
+		quicksort(listadesordenada, j+1, der)
 	}
 }
